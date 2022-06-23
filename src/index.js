5e48697fbd3b427587a3bf42a921581ff69e8398
Andrii <zoops0mania@gmail.com>
2022-06-24 00:34:18 +0300

    feat: ez breezy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+  if (n > 0) {
+  	let nArray = n.toString().split('').reverse().join('');
+  	nArray.startsWith('0') ? nArray = nArray.slice(1) : nArray; 
+  	return nArray
+  }
+  else if (n < 0) {
+  	let nArray = n.toString().split('').reverse().join('').slice(0,-1);
+  	nArray.startsWith('0') ? nArray = nArray.slice(1) : nArray; 
+  	return nArray
+  }
+  else {
+  	return 0;
+  }
+
 }
